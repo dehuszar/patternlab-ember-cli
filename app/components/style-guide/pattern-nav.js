701768bb3ae2c1f3db0f4aa733808d5d6849b889
Samuel Allen <dehuszar@gmail.com>
2015-03-15 15:29:30 -0500

    upgraded ember-cli to 0.2.0 and cleaned up patternlab style-guide markup to use stock css file for more portability across projects

--- app/components/style-guide/pattern-nav.js
+++ app/components/style-guide/pattern-nav.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    tagName: 'ol',
+    classNames: ['sg-nav'],
     didInsertElement: function() {
         this.$('.sg-acc-handle').on('click', function() {
             Ember.$(this).toggleClass('active');
